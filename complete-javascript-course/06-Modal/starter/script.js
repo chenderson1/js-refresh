ac01f013195f788ac6a2d77bdc0076751f7cb25d
Cory Henderson <cory.henderson07@gmail.com>
2022-01-30 11:50:55 -0800

    video_81

--- complete-javascript-course/06-Modal/starter/script.js
+++ complete-javascript-course/06-Modal/starter/script.js
@@ -1,9 +1,20 @@
 'use strict';
+// =======================
 
+// Elements
+
+// ========================
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnOpenModal = document.querySelectorAll('.show-modal');
+
+// ======================
+
+// EVENTS
+
+// ======================
+
 // loop thru nodelist of buttons
 for (let i = 0; i < btnOpenModal.length; i++)
   // show modal on click each btn
@@ -13,6 +24,14 @@ btnCloseModal.addEventListener('click', closeModal);
 document;
 //close on click outside of element
 overlay.addEventListener('click', closeModal);
+//cloce on press escape key
+document.addEventListener('keydown', escapeModal);
+
+// =========================
+
+// FUNCTIONS
+
+// ==========================
 
 function showModal() {
   modal.classList.remove('hidden');
@@ -21,4 +40,10 @@ function showModal() {
 function closeModal() {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
+}
+function escapeModal(event) {
+  const keyName = event.key;
+  if (keyName === 'Escape') {
+    closeModal();
+  }
 }
