44974f94b9134eee7fc3ff74ab2240860936b897
Cory Henderson <cory.henderson07@gmail.com>
2022-01-30 16:40:18 -0800

    refactor

--- complete-javascript-course/06-Modal/starter/script.js
+++ complete-javascript-course/06-Modal/starter/script.js
@@ -1,49 +1,60 @@
 'use strict';
-// =======================
-
-// Elements
-
-// ========================
-const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.close-modal');
-const btnOpenModal = document.querySelectorAll('.show-modal');
-
-// ======================
-
-// EVENTS
-
-// ======================
-
-// loop thru nodelist of buttons
-for (let i = 0; i < btnOpenModal.length; i++)
-  // show modal on click each btn
-  btnOpenModal[i].addEventListener('click', showModal);
-// close modal on click btn
-btnCloseModal.addEventListener('click', closeModal);
-document;
-//close on click outside of element
-overlay.addEventListener('click', closeModal);
-//cloce on press escape key
-document.addEventListener('keydown', escapeModal);
-
-// =========================
-
-// FUNCTIONS
-
 // ==========================
 
-function showModal() {
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
-}
-function closeModal() {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-}
-function escapeModal(event) {
-  const keyName = event.key;
-  if (keyName === 'Escape') {
-    closeModal();
-  }
-}
+// Click Game
+
+// ============================
+
+// // =======================
+
+// // Elements
+
+// // ========================
+// const modal = document.querySelector('.modal');
+// const overlay = document.querySelector('.overlay');
+// const btnCloseModal = document.querySelector('.close-modal');
+// const btnOpenModal = document.querySelectorAll('.show-modal');
+
+// // ======================
+
+// // EVENTS
+
+// // ======================
+
+// // loop thru nodelist of buttons
+// for (let i = 0; i < btnOpenModal.length; i++)
+//   // show modal on click each btn
+//   btnOpenModal[i].addEventListener('click', showModal);
+// // close modal on click btn
+// btnCloseModal.addEventListener('click', closeModal);
+// document;
+// //close on click outside of element
+// overlay.addEventListener('click', closeModal);
+// //cloce on press escape key
+// document.addEventListener('keydown', escapeModal);
+
+// // =========================
+
+// // FUNCTIONS
+
+// // ==========================
+
+// function showModal() {
+//   modal.classList.remove('hidden');
+//   overlay.classList.remove('hidden');
+// }
+// function closeModal() {
+//   modal.classList.add('hidden');
+//   overlay.classList.add('hidden');
+// }
+// function escapeModal(event) {
+//   const keyName = event.key;
+//   if (keyName === 'Escape') {
+//     closeModal();
+//   }
+// }
+// ==========================
+
+// "Pig" Game
+
+// ============================
