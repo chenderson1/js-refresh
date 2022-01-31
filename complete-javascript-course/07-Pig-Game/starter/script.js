120813683d22d5174d4aae13cdc1487fd7b2fe6d
Cory Henderson <cory.henderson07@gmail.com>
2022-01-30 17:42:29 -0800

    video_82

--- complete-javascript-course/07-Pig-Game/starter/script.js
+++ complete-javascript-course/07-Pig-Game/starter/script.js
@@ -1,1 +1,31 @@
 'use strict';
+
+const dice = document.querySelector('.dice');
+const btnNewGame = document.querySelector('.btn--new');
+const btnRollDice = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.hold');
+let playerOneTotalScore = document.querySelector('#score--0');
+let playerOneCurrentScore = document.querySelector('#current--0');
+let playerTwoTotalScore = document.querySelector('#score--1');
+let playerTwoCurrentScore = document.querySelector('#current--1');
+let randomDice;
+
+btnRollDice.addEventListener('click', rollDice);
+btnNewGame.addEventListener('click', gameRestart);
+
+function gameRestart() {
+  playerOneTotalScore.textContent = 0;
+  playerTwoTotalScore.textContent = 0;
+  dice.classList.add('hidden');
+}
+
+function rollDice() {
+  randomDice = `dice-${Math.trunc(Math.random() * 6) + 1}.png`;
+  dice.src = randomDice;
+  dice.classList.remove('hidden');
+}
+
+// function didRollOne() {
+//   if (randomNum === 1) {
+//   }
+// }
